e5e99e1fab11814a2efcd64fbdf46ab15d228652
Patrick Soares <106115172+pscamposs@users.noreply.github.com>
2024-11-20 23:10:19 -0300

    refactor: simplify ContentContainer by removing ModalContextProvider and adjusting height

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -5,15 +5,11 @@ import ModalContextProvider from "../context/use-modal-context";
 
 const Container = styled.div`
   padding: 4rem 16px;
-  height: 100dvh;
+  height: fit-content;
   transition: all 0.2s ease-in;
   overflow-y: scroll;
 `;
 
 export function ContentContainer({ children }: { children: React.ReactNode }) {
-  return (
-    <ModalContextProvider>
-      <Container>{children}</Container>
-    </ModalContextProvider>
-  );
+  return <Container>{children}</Container>;
 }
