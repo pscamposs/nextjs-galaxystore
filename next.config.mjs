0a92df55f33114e01b689ac88f763640417db535
Patrick Soares <106115172+pscamposs@users.noreply.github.com>
2024-11-20 21:23:31 -0300

    fix: remove sensitive environment variables from configuration

--- next.config.mjs
+++ next.config.mjs
@@ -20,10 +20,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    API_URL: "https://galaxy.patrickdev.me",
-    NEXTAUTH_SECRET: "28087d7e0010b9179fb5687b2031dc481a4455cb",
-  },
 };
 
 export default nextConfig;
