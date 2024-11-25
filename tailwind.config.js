56e8e180948c9273cfb28ec8458e241bbe5a8881
Patrick Soares <106115172+pscamposs@users.noreply.github.com>
2024-11-24 22:31:51 -0300

    feat: adicionar animação de flutuação ao tema do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      float: {
+        "0%%": { transform: "translateY(0)" },
+        "50%": { transform: "translateY(15px)" },
+        "100%": { transform: "translateY(0)" },
+      },
+      animation: {
+        float: "float 5s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
