8b92c8ae19bfb245c359d60261b1a32701a160dc
Patrick Soares <106115172+pscamposs@users.noreply.github.com>
2024-12-03 20:45:07 -0300

    feat: adicionar componente ModalTabs com funcionalidade de troca de abas e integração ao carrinho

--- src/components/modal/ModalTabs.tsx
+++ src/components/modal/ModalTabs.tsx
@@ -1,30 +1,103 @@
-import { useContext } from "react";
-import styled from "styled-components";
+import { useContext, useState } from "react";
 import { ModalContext } from "../../context/use-modal-context";
+import GeneralContent from "./(tabs)/GeneralContent";
+import CommandsContent from "./(tabs)/CommandsContent";
+import PermissionsContent from "./(tabs)/PermissionContent";
+import UpdatesContent from "./(tabs)/UpdatesContent";
+import Image from "next/image";
+import { centsToReal } from "@/utils/FormatUtils";
+import { LoaderButton } from "../LoaderButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShop } from "@fortawesome/free-solid-svg-icons";
+import { Plugin } from "@/types/FilterTypes";
+import { fetchClient } from "@/libs/fetchClient";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const ModalTabContainer = styled.button<{ selected: boolean }>`
-  display: inline-block;
-  border: none;
-  margin-left: 8px;
-  padding: 8px 6px;
-  border-bottom: 1px solid
-    ${(props) => (props.selected ? "var(--draft-color);" : "#000")};
-  cursor: pointer;
-  background-color: transparent;
-  color: var(--primary-white);
-  font-size: 16px;
-  transition: 0.2s all ease-in;
-`;
+export default function ModalTabs() {
+  const { tab, toggleTab, plugin } = useContext(ModalContext);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-export default function ModalTab({ target }: { target: string }) {
-  const { tab, toggleTab } = useContext(ModalContext);
+  const addItem = async (plugin?: Plugin) => {
+    setLoading(true);
+    const response = await fetchClient(
+      `/cart/${plugin?.id}`,
+      {
+        method: "PUT",
+      },
+      false
+    );
+    if (response.ok) {
+      toast.success("Plugin adicionado ao seu carrinho.");
+    } else if (response.status == 409) {
+      const data = await response.json();
+      toast.error(data.message);
+    } else {
+      router.push("/login");
+    }
+    setLoading(false);
+  };
+  const tabs = {
+    Geral: <GeneralContent />,
+    Comandos: <CommandsContent />,
+    Perms: <PermissionsContent />,
+    Updates: <UpdatesContent />,
+  };
 
+  const handleChangeTab = (target: string) => {
+    const view = Object.entries(tabs).filter((t) => t[0] == target)[0];
+    toggleTab(view);
+  };
   return (
-    <ModalTabContainer
-      selected={tab === target}
-      onClick={() => toggleTab(target)}
-    >
-      {target}
-    </ModalTabContainer>
+    <div>
+      <div>
+        <div className="flex items-center gap-4 cursor-default py-8 mb-4">
+          <div className="bg-zinc-900 p-4">
+            <Image
+              width={64}
+              height={64}
+              src={plugin?.image || ""}
+              alt="pluginImage"
+            />
+          </div>
+          <div className="flex flex-col justify-between h-24">
+            <div>
+              <h2 className="text-slate-400">{plugin?.category.name}</h2>
+              <p className="font-bold text-2xl">{plugin?.name}</p>
+            </div>
+            <div>
+              <span className="text-zinc-300">
+                {plugin?.downloads} downloads
+              </span>
+              <p className="text-zinc-100">{centsToReal(plugin?.price || 0)}</p>
+              <LoaderButton
+                onClick={() => addItem(plugin)}
+                loading={loading}
+                className="py-2"
+              >
+                <FontAwesomeIcon icon={faShop} className="mx-2" />
+                Comprar
+              </LoaderButton>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-4 py-2 border-b border-zinc-700">
+        {Object.entries(tabs).map(([key, value]) => {
+          return (
+            <button
+              className={`font-light border-b-2 text-sm border-zinc-700 ${
+                tab.label === key && "border-purple-500"
+              } transition-all hover:text-purple-700 `}
+              onClick={() => handleChangeTab(key)}
+              key={key}
+            >
+              {key}
+            </button>
+          );
+        })}
+      </div>
+    </div>
   );
 }
