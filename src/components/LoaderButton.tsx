c8c34cdcc8c991193fb8fe9c334760e6deb5dd4a
Patrick Soares <106115172+pscamposs@users.noreply.github.com>
2024-12-03 20:43:53 -0300

    feat: substituir componente de carregamento no botão por uma implementação com classes utilitárias

--- src/components/LoaderButton.tsx
+++ src/components/LoaderButton.tsx
@@ -1,26 +1,4 @@
 import React from "react";
-import styled from "styled-components";
-
-const ButtonLoader = styled.div`
-  padding: 8px 0;
-  border: 2px solid #fff;
-  border-bottom: 2px solid #222;
-  width: 20px;
-  height: 20px;
-  margin: 0 auto;
-  border-radius: 50%;
-  animation: spin 4s infinite;
-
-  @keyframes spin {
-    0% {
-      transform: rotate(0);
-    }
-
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-`;
 
 export interface IButton
   extends React.DetailedHTMLProps<
@@ -35,7 +13,11 @@ export const LoaderButton: React.FC<IButton> = (props) => {
   const { children, loading, ...rest } = props;
   return (
     <button {...rest}>
-      {loading ? <ButtonLoader></ButtonLoader> : <>{children}</>}
+      {loading ? (
+        <div className="m-auto p-4 rounded-full w-2 animate-spin border-2 border-b-transparent border-white"></div>
+      ) : (
+        <>{children}</>
+      )}
     </button>
   );
 };
